8fe1fda1550b3d5f0952a5330dfea2d1e0909c68
capslock321 <rzeng1522@gmail.com>
2023-08-30 17:59:34 -0400

    add and use external dependencies

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# perlin_impl
+# PerlinImpl
+A C++ implementation of Ken Perlin's Perlin Noise
--- tests/main.cpp
+++ tests/main.cpp
@@ -3,6 +3,8 @@
 #include <format>
 #include <algorithm>
 
+#include "../external/png++/png.hpp"
+
 #include "../src/perlin.hpp"
 
 /*
@@ -26,6 +28,19 @@ void write_ppm(std::vector<double> image, std::string fp, int w, int h) {
     file.close();
 }
 
+void write_png(std::vector<double> noise, std::string fp, int w, int h) {
+    png::image<png::rgb_pixel> image(w, h);
+    for (size_t y = 0; y < image.get_height(); ++y) {
+        for (size_t x = 0; x < image.get_width(); ++x) {
+            double color_value = (noise.at(x + y * w) + 1) / 2;
+
+            int color = color_value * 255;
+            image.set_pixel(x, y, png::rgb_pixel(color, color, color));
+        }
+    }
+    return image.write(fp);
+}
+
 
 int main(int argc, char* argv[]) {
     PerlinImpl::Perlin2D perlin(256, 256, 1992);
@@ -39,7 +54,8 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    write_ppm(noise, "../../noise.ppm", perlin.width, perlin.height);
+    // write_ppm(noise, "../../noise.ppm", perlin.width, perlin.height);
+    write_png(noise, "../../noise.ppm", perlin.width, perlin.height);
 
     std::cout << "Done!" << "\n";
     return 0;
